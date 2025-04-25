be39ec3e3de6c412903b5c9e1d70af7f6aab8d6f
Fabrizio Malcangi <fmalcangi83@gmail.com>
2025-04-25 19:24:21 +0200

    mod varie

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'))
+
 // Import delle rotte
 const routeComm = require('./routes/commerciale')
 const routeProd = require('./routes/produzione')
 
 // app.set('views', './views')
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('homepage', { pageTitle: 'Studio5 Home Page', nameUser: 'Fabrizio il re' })
+})
 
 app.use(routeComm)
 app.use(routeProd)
